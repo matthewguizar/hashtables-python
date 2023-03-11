1252b55dd51b9898601aed8f96772f8beeb6d73d
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-11 07:17:56 -0800

    keys method that returns all keys

--- hashtables.py
+++ hashtables.py
@@ -26,6 +26,14 @@ class HashTable:
                     return self.data_map[index][i][1]
             return None
 
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
 
 my_hash_table = HashTable()
 
@@ -33,7 +41,9 @@ my_hash_table.set_item('bolts', 1400)
 my_hash_table.set_item('washers', 50)
 my_hash_table.set_item('lumber', 70)
 
-print(my_hash_table.get_item("lumber"))
 
 my_hash_table.print_table()
+
+print(my_hash_table.get_item("lumber"))
+print(my_hash_table.keys())
 
